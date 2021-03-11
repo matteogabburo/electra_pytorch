5e520093e1a54108f3fcfb1d7655d30f76d1d388
matteogabburo <matteogabburo@gmail.com>
2021-03-11 17:13:43 +0100

    to py3 and formatted

--- _utils/wsc_ijiri.py
+++ _utils/wsc_ijiri.py
@@ -13,62 +13,88 @@
 # dev 67 -> test 66.4
 
 if c.wsc_ijiri:
-  from _utils.wsc_trick import *
-  equal_token_id = hf_tokenizer.convert_tokens_to_ids(['='])[0]
-  def wsc_preprocess(examples, augment=False):
-    new_examples = {'inp_ids':[], 'attn_mask':[], 'token_type_ids':[], 'label':[]}
-    for i in range(len(examples['span1_text'])):
-      # clean up the candidate
-      the_candidate = examples['span1_text'][i]
-      the_candidate = the_candidate.replace('\n',' ')
-      if the_candidate.endswith('.') or the_candidate.endswith(','):
-        the_candidate = the_candidate[:-1]
-      # clean up pronoun
-      pronoun = examples['span2_text'][i].rstrip('.,"')
-      # find other pronounce
-      if augment:
-        cand_spans = filter_noun_chunks(
-                  extended_noun_chunks(spnlp(examples['text'][i])),
-                  exclude_pronouns=True,
-                  exclude_query=the_candidate,
-                  exact_match=False,
-              )
-        other_candidates = [str(span) for span in cand_spans]
-      else:
-        other_candidates = []
+    from _utils.wsc_trick import *
 
-      for candidate, label in zip([the_candidate, *other_candidates], [examples['label'][i]] + [0]*len(other_candidates)):
-        # random choose
-        select = random.randint(0,1)
-        # 1
-        if select==0 or augment:
-          query = f'{pronoun} = {candidate} ?'
-          sample = hf_tokenizer.encode_plus(query, examples['text'][i])
-          new_examples['inp_ids'].append(sample['input_ids'])
-          new_examples['attn_mask'].append(sample['attention_mask'])
-          new_examples['token_type_ids'].append(sample['token_type_ids'])
-          new_examples['label'].append(label)
-        # 2
-        if select==1 or augment:
-          query = f'{candidate} = {pronoun} ?'
-          sample = hf_tokenizer.encode_plus(examples['text'][i], query)
-          new_examples['inp_ids'].append(sample['input_ids'])
-          new_examples['attn_mask'].append(sample['attention_mask'])
-          new_examples['token_type_ids'].append(sample['token_type_ids'])
-          new_examples['label'].append(label)
-    return new_examples
+    equal_token_id = hf_tokenizer.convert_tokens_to_ids(["="])[0]
 
-  wsc = datasets.load_dataset('super_glue', 'wsc', cache_dir='./datasets')
-  #bk()
-  glue_dsets['wnli'] = wsc.my_map(wsc_preprocess, batched=True, 
-                                  remove_columns=wsc['train'].column_names,
-                                  fn_kwargs={'train':{'augment': True}},
-                                  cache_file_names="ijiri_{split}.arrow")
-  hf_dsets = HF_Datasets(glue_dsets['wnli'], hf_toker=hf_tokenizer, n_inp=3,
-                cols={'inp_ids':TensorText, 'attn_mask':noop, 'token_type_ids':noop, 'label':TensorCategory})
-  glue_dls['wnli'] = hf_dsets.dataloaders(bs=32, shuffle_train=True, num_workers=c.num_workers,
-                                        cache_name="dl_ijiri_{split}.json")
-  LOSS_FUNC['wnli'] = LabelSmoothingCrossEntropyFlat()
+    def wsc_preprocess(examples, augment=False):
+        new_examples = {
+            "inp_ids": [],
+            "attn_mask": [],
+            "token_type_ids": [],
+            "label": [],
+        }
+        for i in range(len(examples["span1_text"])):
+            # clean up the candidate
+            the_candidate = examples["span1_text"][i]
+            the_candidate = the_candidate.replace("\n", " ")
+            if the_candidate.endswith(".") or the_candidate.endswith(","):
+                the_candidate = the_candidate[:-1]
+            # clean up pronoun
+            pronoun = examples["span2_text"][i].rstrip('.,"')
+            # find other pronounce
+            if augment:
+                cand_spans = filter_noun_chunks(
+                    extended_noun_chunks(spnlp(examples["text"][i])),
+                    exclude_pronouns=True,
+                    exclude_query=the_candidate,
+                    exact_match=False,
+                )
+                other_candidates = [str(span) for span in cand_spans]
+            else:
+                other_candidates = []
+
+            for candidate, label in zip(
+                [the_candidate, *other_candidates],
+                [examples["label"][i]] + [0] * len(other_candidates),
+            ):
+                # random choose
+                select = random.randint(0, 1)
+                # 1
+                if select == 0 or augment:
+                    query = f"{pronoun} = {candidate} ?"
+                    sample = hf_tokenizer.encode_plus(query, examples["text"][i])
+                    new_examples["inp_ids"].append(sample["input_ids"])
+                    new_examples["attn_mask"].append(sample["attention_mask"])
+                    new_examples["token_type_ids"].append(sample["token_type_ids"])
+                    new_examples["label"].append(label)
+                # 2
+                if select == 1 or augment:
+                    query = f"{candidate} = {pronoun} ?"
+                    sample = hf_tokenizer.encode_plus(examples["text"][i], query)
+                    new_examples["inp_ids"].append(sample["input_ids"])
+                    new_examples["attn_mask"].append(sample["attention_mask"])
+                    new_examples["token_type_ids"].append(sample["token_type_ids"])
+                    new_examples["label"].append(label)
+        return new_examples
+
+    wsc = datasets.load_dataset("super_glue", "wsc", cache_dir="./datasets")
+    # bk()
+    glue_dsets["wnli"] = wsc.my_map(
+        wsc_preprocess,
+        batched=True,
+        remove_columns=wsc["train"].column_names,
+        fn_kwargs={"train": {"augment": True}},
+        cache_file_names="ijiri_{split}.arrow",
+    )
+    hf_dsets = HF_Datasets(
+        glue_dsets["wnli"],
+        hf_toker=hf_tokenizer,
+        n_inp=3,
+        cols={
+            "inp_ids": TensorText,
+            "attn_mask": noop,
+            "token_type_ids": noop,
+            "label": TensorCategory,
+        },
+    )
+    glue_dls["wnli"] = hf_dsets.dataloaders(
+        bs=32,
+        shuffle_train=True,
+        num_workers=c.num_workers,
+        cache_name="dl_ijiri_{split}.json",
+    )
+    LOSS_FUNC["wnli"] = LabelSmoothingCrossEntropyFlat()
 
 # if c.wsc_ijiri:
 #   equal_token_id = hf_tokenizer.convert_tokens_to_ids(['='])[0]
@@ -118,7 +144,7 @@ if c.wsc_ijiri:
 #       return self.criterion(scores.float(), y.float())
 #     def decodes(self, scores):
 #       return scores <= 0
-      
+
 #   def wsc_ijiri_accuracy(scores, targs):
 #     predicts = scores <= 0
 #     return (predicts == targs).float().mean()
